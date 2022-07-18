acf66fd3d795df2e4d264dd6f69e3d2fd06db260
enot <enotsone@ya.ru>
2022-07-18 23:51:04 +0300

    8.1 and 8.2

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            #region //Act 8.1 
+            /*
             DirectoryInfo dir = new DirectoryInfo("F:/C#/ConsoleApp1");
             Console.WriteLine("============Список каталогов=============");
             foreach (var item in dir.GetDirectories())
@@ -26,7 +28,35 @@ namespace ConsoleApp2
             {
                 Console.WriteLine(item.Name);
             }
-            Console.ReadLine();
+            Console.ReadLine();  */
+            #endregion
+            #region //Act 8.2
+            string path = "F:/C#/Calc.txt";
+            /*if (!File.Exists(path));
+            {
+                File.Create(path);
+            }*/
+            Random rnd = new Random();
+            
+            StreamWriter sw = new StreamWriter(path,true);
+            for (int i = 0; i < 10; i++)
+            {
+                sw.WriteLine(rnd.Next(100));
+                
+            }
+            sw.Close();
+
+            StreamReader sr = new StreamReader(path);
+            int Sum = new int();
+            for (int i = 0; i < 10; i++)
+            {
+                Sum = Sum + Convert.ToInt32(sr.ReadLine());
+
+            }
+            Console.WriteLine(Sum);
+            Console.ReadKey();
+            #endregion
+
         }
     }
 }
